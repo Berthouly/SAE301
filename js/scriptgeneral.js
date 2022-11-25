bba3be5b8a00642576cbba336fe6e70a95794804
Jean-Baptiste Berthouly <100587933+Berthouly@users.noreply.github.com>
2022-11-25 19:56:38 +0100

    Delete js directory

--- js/scriptgeneral.js
+++
@@ -1,8 +0,0 @@
-liste = document.cookie //recupere le cookie  sous forme de chaine de caractere 
-if (liste.length!=0)montab = JSON.parse(liste) // transforme la chaine  en tableau JSON
-else montab =Array() // si il n'y a pas de tableau dans le cookie alors créer le tableau
-console.log(montab)
-
-panier = 0
-montab.forEach(element => {   panier += 1*element.quantite })
-document.getElementById('panier').innerHTML = panier
